f5b14439f1b035309f679446e4ebdacacf36fa8c
Mark Zolotas <mark@cosmic-ocean.prl>
2017-11-01 09:13:28 +0000

    Adding arguments to multi shell script

--- vae/run_multi_model.sh
+++ vae/run_multi_model.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "VAE type is: $1"
+echo "Latent dim is: $2"
 
 for NUM in `seq 0 1 9`
 do
-  python train_multi_vae.py --index $NUM --vae_type=$1
+  python train_multi_vae.py --index $NUM --vae_type=$1 --latent_dim=$2
 done
